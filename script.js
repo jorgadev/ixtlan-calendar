9651d9074789c4b9a442e93a2ea8bbc6e7eba63b
unknown <radovan.jorgic@viberate.com>
2023-09-28 10:42:46 +0200

    Select and input

--- script.js
+++ script.js
@@ -1,76 +1,31 @@
-const calendarContainer = document.querySelector(".calendar-container");
 const calendarGrid = document.querySelector(".calendar-grid");
-const calendarMonth = document.querySelector(".calendar-month");
-const calendarYear = document.querySelector(".calendar-year");
 
-const calendarJumpBtn = document.querySelector(".btn-jump");
+const monthSelect = document.querySelector(".month-select");
+const yearInput = document.querySelector(".year-input");
+const jumpInput = document.querySelector(".jump-input");
+
 const calendarToggleBtn = document.querySelector(".btn-toggle");
-const calendarPrevBtn = document.querySelector(".btn-prev");
-const calendarNextBtn = document.querySelector(".btn-next");
+const calendarJumpBtn = document.querySelector(".btn-jump");
 
-// STATE
-let selectedDate = new Date();
-let selectedYear = selectedDate.getFullYear();
-let selectedMonthIndex = selectedDate.getMonth();
-let calendarShown = false;
+// State & Listeners
+const today = new Date();
+let selectedDay = today.getDate();
+let selectedMonthIndex = today.getMonth();
+let selectedYear = today.getFullYear();
+const holidays = {};
 
-// INITIAL SETUP & LISTENERS
-calendarToggleBtn.addEventListener("click", () => {
-  calendarShown = !calendarShown;
-  setCalendar();
-});
+calendarJumpBtn.addEventListener("click", jumpBtnHandler);
+calendarToggleBtn.addEventListener("click", toggleBtnHandler);
 
-calendarPrevBtn.addEventListener("click", () => {
-  selectedMonthIndex = selectedMonthIndex === 0 ? 11 : selectedMonthIndex - 1;
-  selectedYear = selectedMonthIndex === 0 ? selectedYear - 1 : selectedYear;
-  setCalendar();
-});
+fetchHolidays();
+changeDate(selectedDay, selectedMonthIndex + 1, selectedYear);
 
-calendarNextBtn.addEventListener("click", () => {
-  selectedMonthIndex = selectedMonthIndex === 11 ? 0 : selectedMonthIndex + 1;
-  selectedYear = selectedMonthIndex === 0 ? selectedYear + 1 : selectedYear;
-  setCalendar();
-});
-
-setCalendar();
-
-// FUNCTIONS
-function setCalendar() {
-  calendarMonth.textContent = MONTHS[selectedMonthIndex];
-  calendarYear.textContent = selectedYear;
-  calendarToggleBtn.textContent = calendarShown ? "Close" : "Open";
-  calendarContainer.style.display = calendarShown ? "block" : "none";
-
-  if (calendarShown) {
-    fillCalendarGrid();
-  }
-}
-
-function selectDate(e) {
-  const el = e.target;
-  const activeBefore = document.querySelector(".active");
-
-  if (activeBefore) {
-    activeBefore.classList.remove("active");
-  }
-
-  el.classList.add("active");
-}
-
-function getFirstDayIndex(year, month) {
-  return new Date(year, month, 0).getDay();
-}
-
-function getDaysInMonth(year, month) {
-  return new Date(year, month, 0).getDate();
-}
-
-// 1: FILL THE CALENDAR GRID
+// Calendar functions
 function fillCalendarGrid() {
   calendarGrid.innerHTML = "";
 
-  const firstDayIndex = getFirstDayIndex(selectedYear, selectedMonthIndex);
-  const daysInMonth = getDaysInMonth(selectedYear, selectedMonthIndex + 1);
+  const firstDayIndex = getFirstDayOfMonth(selectedYear, selectedMonthIndex);
+  const daysInMonth = getDaysInMonth(selectedYear, selectedMonthIndex);
 
   let day = 1;
   for (let i = 0; i < 42; i++) {
@@ -82,12 +37,12 @@ function fillCalendarGrid() {
     } else {
       calendarDay.classList.add(i);
       calendarDay.textContent = day;
-      calendarDay.addEventListener("click", selectDate);
 
       // Check if sunday
       if ([6, 13, 20, 27, 34, 41].includes(i)) {
         calendarDay.classList.add("red");
       }
+
       // Check if vacation
       if (false) {
         calendarDay.classList.add("border");
@@ -98,4 +53,98 @@ function fillCalendarGrid() {
 
     calendarGrid.appendChild(calendarDay);
   }
+}
+
+function jumpBtnHandler() {
+  const value = jumpInput.value;
+
+  const dateFormatRegex =
+    /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(000[1-9]|00[1-9]\d|0[1-9]\d{2}|[1-9]\d{3}|[1-9]\d{4})$/;
+
+  if (dateFormatRegex.test(value)) {
+    const valueArr = value.split("/");
+    const day = parseInt(valueArr[0]);
+    const month = parseInt(valueArr[1]);
+    const year = parseInt(valueArr[2]);
+
+    if (isValidDate(day, month, year)) {
+      changeDate(day, month, year);
+    } else {
+      alert("Invalid date format");
+    }
+  } else {
+    alert("Invalid date format");
+  }
+}
+
+function toggleBtnHandler() {
+  const day = 1;
+  const month = parseInt(monthSelect.value) + 1;
+  const year = parseInt(yearInput.value);
+
+  changeDate(day, month, year);
+}
+
+function changeDate(day, month, year) {
+  selectedDay = day;
+  selectedMonthIndex = month - 1;
+  selectedYear = year;
+
+  const jumpInputValue = new Date(year, month - 1, day).toLocaleDateString(
+    "en-GB",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }
+  );
+
+  jumpInput.value = jumpInputValue;
+  monthSelect.value = month - 1;
+  yearInput.value = year;
+
+  fillCalendarGrid();
+}
+
+// Fetch holidays
+async function fetchHolidays() {
+  try {
+    const response = await fetch("holidays.txt");
+    const lines = await response.text();
+
+    lines.split(/\r\n|\n/).forEach((line) => {
+      const lineArr = line.split(";");
+      const date = lineArr[0];
+      const repeat = lineArr[1] === "true";
+
+      holidays[date] = { repeat: repeat };
+    });
+
+    fillCalendarGrid();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Helpers
+function isValidDate(day, month, year) {
+  const date = new Date(year, month - 1, day);
+
+  return (
+    date.getFullYear() === year &&
+    date.getMonth() === month - 1 &&
+    date.getDate() === day
+  );
+}
+
+function getFirstDayOfMonth(year, monthIndex) {
+  const firstDayOfMonth = new Date(year, monthIndex, 1);
+  let firstDayOfWeek = firstDayOfMonth.getDay();
+  firstDayOfWeek = firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1;
+
+  return firstDayOfWeek;
+}
+
+function getDaysInMonth(year, monthIndex) {
+  return new Date(year, monthIndex + 1, 0).getDate();
 }
